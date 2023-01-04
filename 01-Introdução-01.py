bd8b3142000074bb9dc6dfb693b803765e81027a
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-04 19:17:14 -0300

    .

---
+++ 01-Introdução-01.py
@@ -0,0 +1,13 @@
+print('Olá, Mundo!')
+print(7+4)
+print ('Olá', 5 ,'+', 7, 'é igual a ', 5+7)
+
+nome = 'Matheus'
+idade = '23'
+peso = '70'
+print (nome ,'tem', idade, ' anos e pesa' , peso, '.')
+
+sobrenome = input('Qual seu sobrenome?')
+Nascimento = input('Ano que vc nasceu?')
+Escola = input ('Escola onde estudou?')
+print(sobrenome, Nascimento , Escola) 
