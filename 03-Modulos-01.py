1b6670b7af347378f47ab8fc6473ec96c8950e68
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-07 22:07:31 -0300

    Create 03-Modulos-01.py

---
+++ 03-Modulos-01.py
@@ -0,0 +1,7 @@
+import math
+num = int(input('Digite um numero:'))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {}. '.format(num , raiz))
+
+import emoji
+print(emoji.emojize("Olá, mundo :sunglasses:", use_aliases=True))
