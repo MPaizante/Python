591ac74d30d4458a64054e2e9cc4957d9c3da25a
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-08 17:06:22 -0300

    Create 04-Manipuando-Text-01.py

---
+++ 04-Manipuando-Text-01.py
@@ -0,0 +1,12 @@
+frase = 'Curso em Video Python'
+print()
+print(frase[:14])
+print(frase.count('o'))
+print(len(frase.strip()))
+print('Curso' in frase)
+print(frase.split())
+
+print("""O conto é um tipo de história literária. Geralmente, é uma obra de ficção, ou 
+      seja, de faz de conta, pois retrata um mundo de fantasia a partir da imaginação de quem
+      o escreveu. O conto tem narrador e um enredo, isto é, uma história que vai se desenvolver com começo, meio e fim.""")
+
