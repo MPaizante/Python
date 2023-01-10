ee1b0b45049e5c6421bb776362af43cf63760071
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-10 18:28:23 -0300

    .

---
+++ 06-Cores-Terminal-01.py
@@ -0,0 +1,11 @@
+print('\033[31mMatheus Paizante')
+
+print('\033[31;43mMatheus Paizante')
+
+print('\033[1;31;43mMatheus Paizante')
+
+print('\033[1;31;43mMatheus Paizante\033[m')
+
+print('\033[4;30;45mMatheus Paizante\033[m')
+
+print('\033[7;30mMatheus Paizante\033[m')
--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-d = float(input('Qual distancia?'))
-print('Você está preste a começar uma viagem de {}Km'.format(d))
-if d <= 200:
-    pre1 = d * 0.50
-    print('O preço vai ser R${:.2f}'.format(pre1))
-    
-else:
-    pre = d * 0.45
-    print('O preço vai ser R${:.2f}'.format(pre))
+
+nota1 = 19//2
+nota = 19%2
+print(nota1)
+print(nota)
