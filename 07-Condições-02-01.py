96b28a3eb81f39d9cb9cd46ede8ec8f9cbc849b3
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-10 20:03:41 -0300

    .

--- 07-Condições-02-01.py
+++ 07-Condições-02-01.py
@@ -1,3 +1,18 @@
+
+#Aprovando Emprestimo
+vc = float(input('QUal valor da casa?'))
+Salvc = float(input('Qual salario do comprador?'))
+Avc = float(input('Quantos anos vc quer pagar?'))
+pres = vc/(Avc * 12)
+Salm = (Salvc *30) / 100
+print('Para pagar um casa de {:.2f} em {} anos a prestação será de {:.2f}.'.format(vc , Avc , pres))
+if pres < Salm:
+    print('Aprovado')
+else:
+    print('Negado.')
+    
+    
+#conversor de bases numericas
 nb = int(input('Digite um numero inteiro:'))
 print('''Escolha a base de conversão:
 [1] Binario
