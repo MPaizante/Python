6730bbb255c75c19488faf6fa341336d8b5aa1c7
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-14 21:13:04 -0300

    Update 09-Estrutura-Repetição-01-02.py

--- 09-Estrutura-Repetição-01-02.py
+++ 09-Estrutura-Repetição-01-02.py
@@ -134,3 +134,25 @@ while cont1 <= fimtermo:
 print('Fim')
 
 
+
+#Super PA v3.0
+
+print('Gerador de PA')
+print('=-'*10)
+primeiro1 = int(input('Primeiro termo:'))
+razão1 = int(input('Razão:'))
+termo1 = primeiro1
+cont1 = 1
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont1 <= 10:
+        print('{}'.format(termo1), end=' ')
+        termo1 += razão1
+        cont1 += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer a mais?'))
+print('Fim')
+
+
+
