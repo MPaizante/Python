50daabbca580003c788fdb4f97682a32d04ca834
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-15 23:09:39 -0300

    .

--- 10-Interrompendo-Repetições-01-03.py
+++ 10-Interrompendo-Repetições-01-03.py
@@ -66,7 +66,30 @@ while True:
     print('Vamos jogar novamente...')
 print(f'Game over! Voce venceu {v} vezes.')
     
+#Analise de dados do grupo
+total18 = 0
+totalM = 0
+totalF = 0
+while True:
+    i = int(input('Idade:'))
+    s = ' '
+    while s not in 'MF':
+        s = str(input('Sexo:')).strip().upper()[0]
+    if i >= 18:
+        total18 += 1
+    if s  == 'M':
+        totalM += 1
+    if s == 'F' and i < 20:
+        totalF += 1               
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar ? [S/N]')).strip().upper()[0]   
+print('Acabou')
+print(totalM)
+print(totalF)    
 
-        
+# Estatisticas em produtos
+
+
 
     
