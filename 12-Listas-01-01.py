33a713aeb7970ac8259c024b44a4aecee38a34c8
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-17 22:19:07 -0300

    Create 12-Listas-01-01.py

---
+++ 12-Listas-01-01.py
@@ -0,0 +1,13 @@
+#Listas podem ser mudadas 
+XXXXXX = [8 , 5 ,7 ,1 ,2 ,3 ,4 , 0 ,6]
+XXXXXX.sort(reverse=True)#organizar do maior para o menor e somento o sort fica do menor para o maior.
+print(XXXXXX)
+
+ttt = [2 , 5 , 9, 1]
+ttt [2] = 3 #trocou o 5 pelo 2 
+ttt.append(7) #vai add o 7
+ttt.sort()
+ttt.insert(2 , 0)
+ttt.pop()
+print(ttt)
+print(f'Essa lista tem {len(ttt)} elementos.')
