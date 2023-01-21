a0ee91de3d1996b56f2537db5b7b60c5fbca9e1c
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-20 22:55:38 -0300

    Update 12-Listas-01-02.py

--- 12-Listas-01-02.py
+++ 12-Listas-01-02.py
@@ -1,10 +1,13 @@
-t = list
-t.append(str('Matheus'))
-t.append(int(40))
-g = list()
-g.append(t)
-g.append(t[:])
-t[0] = str('Maria')
-t[1] = int(22)
-g.append(t[:])
-print(g)
+g = [['J' , 19] , ['A' , 33] , ['Jo', 13] , ['Maria' , 45]]
+for p in g:
+    print(p)
+    print(f'{p[0]} tem {p[1]} anos de idade.  ')
+    
+gal = list()
+d = list()
+for c in range(0,5):
+    d.append(str('Nome:'))
+    d.append(int(input('Idade:')))
+    gal.append(d[:])
+    d.clear()
+print(gal)
