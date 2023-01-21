c471f2cf1794ab5c4714e11a40ddfcec21731841
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-21 19:59:20 -0300

    .

--- 12-Listas-01-02.py
+++ 12-Listas-01-02.py
@@ -11,3 +11,65 @@ for c in range(0,3):
     gal.append(d[:])
     
 print(gal)
+
+
+#Exercicios
+#Lista composta e analise de dados.
+temp = []
+princ = []
+mai = men = 0
+while True:
+    temp.append(str(input('Nome:')))
+    temp.append(float(input('Peso:')))
+    if len(princ) == 0:
+        mai = men = temp [1]
+    else:
+        if temp[1] > mai:
+           mai = temp[1] 
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Digite se quer continuar. [S/N]')).upper().strip()
+    if resp in 'N':
+        break
+print(f'Os dados foram {princ}')
+print(f'Ao todo, voce cadastrou {len(princ)} pessoas.')
+print(f'O maior peso foi de {mai}kg' , end=' ')
+for p in princ:
+    if p[1] == mai:
+        print(f'p[0]', end=' ')
+print(f'O menor peso foi {men}kg', end=' ')
+for p[1] in princ:
+    print(f'{p[0]}', end=' ')
+    
+
+#Lista com pares e impares
+
+num = [[] , []]
+valor = 0
+for y in range(1,8):
+    valor = int(input(f'Digite o {y}º valor:'))
+    if valor % 2 ==0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+num[0].sort()
+num[1].sort()
+print(f'Todos os valores foram {valor}')
+print(num)
+print(f'Os valores impares foram: {num[1]}.')
+print(f'Os valores pares foram {num[0]}.')
+
+
+
+#Matriz em pyton
+matriz = [[0 , 0 , 0] , [0 , 0 , 0] , [0 , 0 , 0]]
+for l in range(0,3):
+    for coluna in range(0,3):
+        matriz[l][coluna] = int(input(f'Digite um numero:'))
+for l in range(0,3):
+    for coluna in range(0,3):
+        print(f'[{matriz[l][coluna]:^5}]', end=' ')
+    print()
+        
--- test.py
+++ test.py
@@ -1,19 +1,10 @@
-
-
-#Dividindo valores em varias listas
-n3 = list()
-par = list()
-impar = list()
-while True:
-    n3.append(int(input('Digite um numero: ')))
-    res = str(input('Quer continuar? [S/N] '))
-    if res in 'Nn':
-        break
-for i , v in enumerate(n3):
-    if v % 2 == 0:
-        par.append(v)
-    elif v % 2 == 1:
-        impar.append(v)
-print(f'A lista compelta é {n3}')
-print(f'A lista de par é {par}')
-print(f'A lista de impar é {impar}')
+num = [[] , []]
+valor = 0
+for y in range(1,8):
+    valor = int(input('Digite um valor:'))
+    if valor % 2 ==0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+print(f'Todos os valores foram {valor}')
+print(num)
