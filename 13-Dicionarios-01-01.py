b5f6c2f3dd6b15841178decc09274b3c79551e6c
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-21 22:26:12 -0300

    .

--- 13-Dicionarios-01-01.py
+++ 13-Dicionarios-01-01.py
@@ -1,3 +1,6 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
 pessoas = {'nome' : 'Matheus', 'sexo' : 'M' , 'idade' : '23'}
 pessoas['peso'] = 70
 print (pessoas['idade'])
@@ -14,3 +17,53 @@ Brasil.append(estado)
 Brasil.append(estado2)
 
 print(Brasil[0]['UF'])
+
+estado58 = dict()
+brasil6 = list()
+for c in range(0,3):
+    estado58['uf'] = str(input('Unidade Federativa: '))
+    estado58['sigla'] = str(input('Sigla do Estado: '))
+    brasil6.append(estado58.copy())
+for e in brasil6:
+    print(e)
+    for k, v in e.items():
+        print(f'O campo {k} tem o valor {v}.')    
+print(brasil6)
+
+
+#Dicionario em Python
+aluno = dict()
+aluno['nome'] = str(input('Nome do Aluno:'))
+aluno['media'] = float(input(f'Media de {aluno["nome"]}: '))
+if aluno['media'] >=7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno ['situação'] = 'Recuperção'
+else:
+    aluno['situação'] = 'Reprovado'
+for ch , va in aluno.items():
+    print(f' -{ch} é igual a {va}')
+
+
+#Jogo de Dados em Python
+jogo1 = {   'jogador1': randint(1 , 6),
+            'jogador2': randint(1 , 6),
+            'jogador3': randint(1 , 6),
+            'jogador4': randint(1 , 6)
+}
+print(jogo1)
+print('Valores sorteados:')
+for ke , vi in jogo1.items():
+    print(f'{ke} tirou {vi} no dado.')
+    sleep(1)
+ranking = sorted(jogo1.items(), key=itemgetter(1), reverse=True )
+print(ranking)
+print('Ranking dos jogadores.')
+for ii , vi in enumerate(ranking):
+    print(f'{ii+1}º lugar: {vi[0]} com {v[1]}.')
+
+
+#Cadastro de trabalhador em Python
+
+
+
--- test.py
+++ test.py
@@ -1,21 +1,14 @@
-ficha = list()
-while True:
-    nome6 = str(input('Nome:'))
-    nota5 = float(input('Nota1:'))
-    nota4 = float(input('Nota2:'))
-    media = (nota4 + nota5) / 2
-    ficha.append([nome6 , [nota4 , nota5] , media])
-    re = str(input('Quer continuar ? [S/N]: ')).strip().upper()
-    if re in 'N':
-        break
-print(f'{"No.":<4}{"NOME6":<10}{"MEDIA":>8}')
-for i , a in enumerate(ficha):
-    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
-while True:
-    opc = int(input('Mostrar notas de qual aluno? (999 interrompe):'))
-    if opc == 999:
-        print('Finalizado')
-        break
-    if opc <= len(ficha) -1:
-        print(f'Notas de {ficha[opc][0]}')
-print('Volte sempre.')
+from operator import itemgetter
+from random import randint
+from time import sleep
+jogo1 = {   'jogador1': randint(1 , 6),
+            'jogador2': randint(1 , 6),
+            'jogador3': randint(1 , 6),
+            'jogador4': randint(1 , 6)
+}
+print(jogo1)
+print('Valores sorteados:')
+for ke , vi in jogo1.items():
+    print(f'{ke} tirou {vi} no dado.')
+    sleep(1)
+ranking = sorted(jogo1.items(), key=itemgetter(1) )
