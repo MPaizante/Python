fbf56f3bc8f6298cad09a34ed3874096e6af4e7a
Matheus Paizante <matheus_paizante@hotmail.com>
2023-01-24 21:33:39 -0300

    Update 16-Moluos-Pacotes-01-01.py

--- 16-Moluos-Pacotes-01-01.py
+++ 16-Moluos-Pacotes-01-01.py
@@ -1,26 +1,35 @@
 #Exercitando modulos em python
 
-def aumentar(preço , taxa):
+def aumentar(preço = 0 , taxa = 0):
     res = preço + (preço * (taxa/100))
     return res
 
 
-def diminuir(preço , taxa):
+def diminuir(preço  = 0, taxa = 0):
     res = preço - (preço * (taxa / 100))
     return res
     
-def dobro(preço):
+def dobro(preço = 0):
     res = preço * 2
     return res
 
    
-def metade(preço):
+def metade(preço = 0 ):
     res = preço / 2 
     return res
 
 
+def moeda( preço = 0 , moeda = 'R$'):
+    return f'{moeda}{preço}'.replace('.' , '.')
+    
+
+
 p = float(input('Digite o preço:'))
-print(f'A metade de {p} é {metade(p)}.')
-print(f'O dobro de {p} é {dobro(p)}. ')
-print(f'O aumento de 10% de {p} é {aumentar(p , 10)}. ')
-print(f'O desconto de 10% de {p} é {diminuir(p , 10)}. ')
+print(f'A metade de {moeda(p)} é {moeda(metade(p))}.')
+print(f'O dobro de {moeda(p)} é {moeda(dobro(p))}. ')
+print(f'O aumento de 10% de {moeda(p)} é {moeda(aumentar(p , 10))}. ')
+print(f'O desconto de 10% de {moeda(p)} é {moeda(diminuir(p , 10))}. ')
+print(f'Reduzindo 13% de {moeda(p)}, temos {moeda(diminuir(p , 13))}')
+
+
+#Formatando moeadas
