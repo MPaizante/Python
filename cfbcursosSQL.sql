0911dae8a945c8ee20566fbd4754c17bda9bfbb0
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-01-25 09:57:15 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -1,6 +1,1 @@
-/*create table tipocliente(
-	i_tipocliente_tipocliente int primary key auto_increment,
-    s_dsctipocliente_tipocliente varchar(100) not null
-);
-*/
-alter table cliente add constraint fk_tipocliente foreign key (i_tipo_cliente) references tipocliente (i_tipocliente_tipocliente);
+alter table cliente modify column i_idade_cliente tinyint;
