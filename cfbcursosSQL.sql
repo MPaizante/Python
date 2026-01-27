97ebfdf99b5d5daa1a701b87aabbdf2ffd4d713d
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-01-27 19:50:12 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -1,1 +1,21 @@
-alter table cliente drop column i_idade_cliente;
+/*insert into tipocliente values(01,'Pessoa Fisica');
+insert into tipocliente values(02,'Pessoa Juridica');
+insert into tipocliente values(03,'Pessoa Cliente Especial');
+
+
+
+insert into cliente values (01,'Matheus','00000000000', '1999-01-26',03);
+insert into cliente values (02,'Astrorgilson','11111111111', '1999-01-26',01);
+insert into cliente values (03,'Carlos','22222222222', '1999-01-26',02);
+insert into cliente values (04,'Joao','30000000000', '1999-01-26',03);
+insert into cliente values (05,'Maria','40000000000', '1999-01-26',01);
+insert into cliente values (06,'Pedro','50000000000', '1999-01-26',01);
+insert into cliente values (07,'Vitor','60000000000', '1999-01-26',02);
+insert into cliente values (08,'Gui','00700000000', '1999-01-26',01);
+insert into cliente values (09,'Paulo','80000000000', '1999-01-26',03);
+insert into cliente values (10,'Ester','90000000000', '1999-01-26',01);*/
+
+
+insert into cliente(i_cliente_cliente,s_nome_cliente, s_cpf_cliente ,d_nasc_cliente,i_tipo_cliente) values (11,'Milena','99999999999','2007-07-19',03);
+
+select * from cliente;
