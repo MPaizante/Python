2a0f9d6f5bce3399980056713cf693b8cc4bea41
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-01-27 22:16:03 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -1,21 +1,20 @@
-/*insert into tipocliente values(01,'Pessoa Fisica');
-insert into tipocliente values(02,'Pessoa Juridica');
-insert into tipocliente values(03,'Pessoa Cliente Especial');
+/*insert into cliente_aux(
+i_cliente_cliente, 
+s_nome_cliente,
+s_cpf_cliente,
+d_nasc_cliente,
+i_tipo_cliente) select 
+i_cliente_cliente, 
+s_nome_cliente,
+s_cpf_cliente,
+d_nasc_cliente,
+i_tipo_cliente from cliente;
+select * from cliente_aux;
+
+
+update cliente set s_nome_cliente = 'Matheus P', s_cpf_cliente = '77777777777' where i_cliente_cliente =1;
+select * from cliente;*/
 
 
 
-insert into cliente values (01,'Matheus','00000000000', '1999-01-26',03);
-insert into cliente values (02,'Astrorgilson','11111111111', '1999-01-26',01);
-insert into cliente values (03,'Carlos','22222222222', '1999-01-26',02);
-insert into cliente values (04,'Joao','30000000000', '1999-01-26',03);
-insert into cliente values (05,'Maria','40000000000', '1999-01-26',01);
-insert into cliente values (06,'Pedro','50000000000', '1999-01-26',01);
-insert into cliente values (07,'Vitor','60000000000', '1999-01-26',02);
-insert into cliente values (08,'Gui','00700000000', '1999-01-26',01);
-insert into cliente values (09,'Paulo','80000000000', '1999-01-26',03);
-insert into cliente values (10,'Ester','90000000000', '1999-01-26',01);*/
 
-
-insert into cliente(i_cliente_cliente,s_nome_cliente, s_cpf_cliente ,d_nasc_cliente,i_tipo_cliente) values (11,'Milena','99999999999','2007-07-19',03);
-
-select * from cliente;
