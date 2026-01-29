6346dd18f53f2dcbd67ee6c85ac92ee95bec46d8
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-01-28 22:17:18 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -1,4 +1,4 @@
-select count(v.i_venda_venda), v.d_data_venda as Vendas from venda v group by d_data_venda;
+select count(v.i_venda_venda), v.d_data_venda as Vendas from venda v group by v.d_data_venda having count(v.i_venda_venda)>1;
 
 
 
