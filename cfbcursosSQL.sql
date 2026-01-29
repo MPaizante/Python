942484899ceedc99f1ee6ae0a18f671b66ad258a
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-01-28 21:46:33 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -1,7 +1,7 @@
 select 
 	v.i_venda_venda,c.s_nome_cliente, c.s_cpf_cliente, v.d_data_venda, v.f_valor_venda, tc.s_dsctipocliente_tipocliente
 from venda v
-	inner join cliente c on v.i_cliente_cliente = c.i_cliente_cliente inner join tipocliente tc on c.i_tipo_cliente = tc.i_tipocliente_tipocliente
+	inner join cliente c on c.i_cliente_cliente = v.i_cliente_cliente inner join tipocliente tc on c.i_tipo_cliente = tc.i_tipocliente_tipocliente
 ;
 
 
