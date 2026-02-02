641e93f6d1529ac5c19aefa65cacde3194a0f3f5
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-02-02 18:12:12 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -5,4 +5,4 @@ count - contagem
 round - arredondar
 */
 
-select c.i_cliente_cliente, c.s_nome_cliente, v.i_venda_venda from cliente c inner join venda v on c.i_cliente_cliente = v.i_cliente_cliente;
+select * from cliente c1, cliente c2 where c1.i_tipo_cliente = c2.i_tipo_cliente and c1.s_nome_cliente = 'Matheus P' order by c1.i_tipo_cliente;
