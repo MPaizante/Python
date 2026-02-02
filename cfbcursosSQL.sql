f6b155327131f91027128385fa89ab1921012515
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-02-02 16:57:42 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -4,4 +4,4 @@ avg - media
 count - contagem
 round - arredondar
 */
-select * from cliente where d_nasc_cliente > '2000-01-01';
+select * from venda where d_data_venda between '2020-07-01' and '2020-11-01' and i_cliente_cliente not in (3,10) and f_valor_venda > 20;
