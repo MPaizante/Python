e5c7493dc1f6708259851aa761e772ecca4a95e9
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-02-02 14:27:25 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -4,4 +4,4 @@ avg - media
 count - contagem
 round - arredondar
 */
-select round(avg(f_valor_venda),2) from venda where f_valor_venda > (select min(f_valor_venda)  from venda) and f_valor_venda < (select max(f_valor_venda)  from venda);
+select * from cliente where d_nasc_cliente > '2000-01-01';
