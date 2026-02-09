a8691acf33a0d1ff8808ef8d7713c605e93be35e
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-02-09 19:01:51 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -1,10 +1,15 @@
-DELIMITER $$
-CREATE PROCEDURE canal(in curso varchar(50))
-BEGIN
-	declare x varchar(20);
-    set x = 'Matheus Paizante';
-    select x, curso;
-END $$
-
-DELIMITER ;
-CALL canal('SQL');
+delimiter $$
+create procedure idade(in idCliente int, out idade int, out res varchar(20))
+begin
+	declare dt datetime;
+    set dt = (select d_nasc_cliente from cliente where i_cliente_cliente = idCliente);
+	set idade = year(now()) - year(dt);
+	if(idade >=18) then
+    set res= 'Maior';
+    else
+    set res = 'menor';
+    end if;
+end $$
+delimiter ;
+call idade(1,@idadecliente,@resultado);
+select @idadecliente, @resultado;
