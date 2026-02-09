3404c7188ec6961d40198d1bddd34c3388ea34c5
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2026-02-09 19:33:31 -0300

    Update cfbcursosSQL.sql

--- cfbcursosSQL.sql
+++ cfbcursosSQL.sql
@@ -1,15 +1,15 @@
 delimiter $$
-create procedure idade(in idCliente int, out idade int, out res varchar(20))
+create procedure loopLoop(in max int,out soma int)
 begin
-	declare dt datetime;
-    set dt = (select d_nasc_cliente from cliente where i_cliente_cliente = idCliente);
-	set idade = year(now()) - year(dt);
-	if(idade >=18) then
-    set res= 'Maior';
-    else
-    set res = 'menor';
-    end if;
+declare x int default 0;
+	meuloop : loop
+	if(x >=max) then
+		leave meuloop;
+	end if;
+    set x = x + 1;
+	end loop;
+    set soma = x;
 end $$
 delimiter ;
-call idade(1,@idadecliente,@resultado);
-select @idadecliente, @resultado;
+call loopLoop(50,@ret);
+select @ret;
